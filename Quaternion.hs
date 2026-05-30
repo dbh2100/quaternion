d4efa52cb31fea19a54b5e8ca0d30e072c637a13
dbh2100 <davidbharrison@gmail.com>
2026-05-29 23:08:35 -0400

    Adding negation and subtraction to Haskell module

--- Quaternion.hs
+++ Quaternion.hs
@@ -1,6 +1,8 @@
 module Quaternion 
 ( Quaternion(..)
 , add
+, qNegate
+, qSubtract
 , multiply
 , norm
 ) where
@@ -9,6 +11,12 @@ data Quaternion = Quaternion Float Float Float Float deriving (Eq, Show)
 
 add :: Quaternion -> Quaternion -> Quaternion
 add (Quaternion s1 i1 j1 k1) (Quaternion s2 i2 j2 k2) = Quaternion (s1+s2) (i1+i2) (j1+j2) (k1+k2)
+
+qNegate :: Quaternion -> Quaternion
+qNegate (Quaternion s i j k) = Quaternion (-s) (-i) (-j) (-k)
+
+qSubtract :: Quaternion -> Quaternion -> Quaternion
+qSubtract q1 q2 = add q1 (qNegate q2)
 
 multiply :: Quaternion -> Quaternion -> Quaternion
 multiply (Quaternion s1 i1 j1 k1) (Quaternion s2 i2 j2 k2) =
