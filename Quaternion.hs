8a625771e7bbb2d7c2109c0e159b7b2f5b8ff68f
dbh2100 <davidbharrison@gmail.com>
2026-06-03 18:00:41 -0400

    Overloading operators

--- Quaternion.hs
+++ Quaternion.hs
@@ -1,30 +1,33 @@
 module Quaternion 
 ( Quaternion(..)
-, add
-, qNegate
-, qSubtract
-, multiply
-, norm
 ) where
 
 data Quaternion = Quaternion Float Float Float Float deriving (Eq, Show)
 
-add :: Quaternion -> Quaternion -> Quaternion
-add (Quaternion s1 i1 j1 k1) (Quaternion s2 i2 j2 k2) = Quaternion (s1+s2) (i1+i2) (j1+j2) (k1+k2)
+instance Num Quaternion where
 
-qNegate :: Quaternion -> Quaternion
-qNegate (Quaternion s i j k) = Quaternion (-s) (-i) (-j) (-k)
+    (+) :: Quaternion -> Quaternion -> Quaternion
+    (Quaternion s1 i1 j1 k1) + (Quaternion s2 i2 j2 k2) = Quaternion (s1+s2) (i1+i2) (j1+j2) (k1+k2)
 
-qSubtract :: Quaternion -> Quaternion -> Quaternion
-qSubtract q1 q2 = add q1 (qNegate q2)
+    negate :: Quaternion -> Quaternion
+    negate (Quaternion s i j k) = Quaternion (-s) (-i) (-j) (-k)
 
-multiply :: Quaternion -> Quaternion -> Quaternion
-multiply (Quaternion s1 i1 j1 k1) (Quaternion s2 i2 j2 k2) =
-    let s = s1*s2 - i1*i2 - j1*j2 - k1*k2
-        i = s1*i2 + i1*s2 + j1*k2 - k1*j2
-        j = s1*j2 - i1*k2 + j1*s2 + k1*i2
-        k = s1*k2 + i1*j2 - j1*i2 + k1*s2
-    in Quaternion s i j k
+    (-) :: Quaternion -> Quaternion -> Quaternion
+    q1 - q2 = q1 + negate q2
 
-norm :: Quaternion -> Float
-norm (Quaternion s i j k) = s^2 + i^2 + j^2 + k^2
+    abs :: Quaternion -> Quaternion
+    abs (Quaternion s i j k) = Quaternion (sqrt (s^2 + i^2 + j^2 + k^2)) 0 0 0
+
+    signum :: Quaternion -> Quaternion
+    signum (Quaternion s i j k) = Quaternion (signum s) (signum i) (signum j) (signum k)
+
+    fromInteger :: Integer -> Quaternion
+    fromInteger n = Quaternion (fromInteger n) 0 0 0
+
+    (*) :: Quaternion -> Quaternion -> Quaternion
+    (Quaternion s1 i1 j1 k1) * (Quaternion s2 i2 j2 k2) =
+        let s = s1*s2 - i1*i2 - j1*j2 - k1*k2
+            i = s1*i2 + i1*s2 + j1*k2 - k1*j2
+            j = s1*j2 - i1*k2 + j1*s2 + k1*i2
+            k = s1*k2 + i1*j2 - j1*i2 + k1*s2
+        in Quaternion s i j k
